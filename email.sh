b44dbca6bec48a9cb6738953cf0c4e59536dad0a
Mateus Lippi Pereira <mateuslippi01@gmail.com>
2022-10-28 14:17:21 -0300

    Primeiro teste.

--- email.sh
+++ email.sh
@@ -27,64 +27,107 @@ MENSAGEM_USO="
         -v - Versão do programa
         -a - Realiza o backup em todas as empresas
         -b - Realiza o backup da Bradok
-        -d - Reliza o backup da DadyIlha
-        -m - Reliza o backup da Mac-id
-"
-CONTAS=($(cat contas.txt))
-EXIST_USER=($(cat /etc/passwd | cut -d . -f 1,2 | uniq -u))
+        -d - Realiza o backup da DadyIlha
+        -m - Realiza o backup da Mac-id
+"DADY
+USERFILE=/tmp/userlist
+USERNAME=$(cat /tmp/userlist | tr 'A-Z'  'a-z')
+PASSWORD='6$5Jtt/TaEHQZoHUeW$Fdyuk3rKUO6eYQPIdnT2PYiZ.9qyXxyiPT7FLehKPZthIrUvy8Ts2.qWlkTq4ZpY0MRvKnp4mv4PVd0LFC.nW1'
 
 
 VERSAO="v1.0"
 CHAVE_BRADOK=0
 CHAVE_DADY=0
 CHAVE_MAC=0
+CHAVE_ALL=0s
 
 #-----------------------TESTES--------------------------------------------- #
 
-
 #-----------------------FUNÇÕES-------------------------------------------- #
 
-checar_usuario() {
-for i in "${EXIST_USER[@]}"; do
-  for j in "${CONTAS[@]}"; do
-    if [ "${CONTAS[i]}" -eq "${EXIST_USER[j]}" ]; then
-      echo "Valor $i encontrado"
-    fi
-  done
-done
-}
-
 criar_usuario_mac() {
-  for i in $CONTAS; do
-     useradd -m                       \
-             -d /home/$i'.mac-id.bkp' \
-             -p '$6$5Jtt/TaEHQZoHUeW$Fdyuk3rKUO6eYQPIdnT2PYiZ.9qyXxyiPT7FLehKPZthIrUvy8Ts2.qWlkTq4ZpY0MRvKnp4mv4PVd0LFC.nW1' \;
-             # Abc242526@2
+  for i in $USERNAME; do
+     useradd -m                         \
+             -d /home/$i'.mac-id.bkp'   \
+             -p $PASSWORD               \;
+
   done
 
 }
 
 criar_usuario_dady() {
-  for i in $CONTAS; do
-     useradd -m                       \
+  for i in $USERNAME; do
+     useradd -m                         \
              -d /home/$i'.dadyilha.bkp' \
-             -p '$6$5Jtt/TaEHQZoHUeW$Fdyuk3rKUO6eYQPIdnT2PYiZ.9qyXxyiPT7FLehKPZthIrUvy8Ts2.qWlkTq4ZpY0MRvKnp4mv4PVd0LFC.nW1' \;
-             # Abc242526@2
+             -p $PASSWORD               \;
   done
 
 }
 
 criar_usuario_bradok() {
-  for i in $CONTAS; do
-     useradd -m                       \
-             -d /home/$i'.bradok.bkp' \
-             -p '$6$5Jtt/TaEHQZoHUeW$Fdyuk3rKUO6eYQPIdnT2PYiZ.9qyXxyiPT7FLehKPZthIrUvy8Ts2.qWlkTq4ZpY0MRvKnp4mv4PVd0LFC.nW1' \;
-             # Abc242526@2
+  for i in $USERNAME; do
+     useradd -m                        \
+             -d /home/$i'.bradok.bkp'  \
+             -p $PASSWORD              \;
   done
 
 }
 
+email_dady() {
+  while read p; do
+          imapsync --host1 'imap.umbler.com'              \
+                   --user1 $p'@dadyilha.com.br'           \
+                   --password1 '@?SueTam#2' --ssl1 --sslargs1 "SSL_verify_mode=1"  \
+                   --host2 'localhost'                    \
+                   --user2 $p'.dadyilha.bkp'              \
+                   --password2 'Abc242526@2'              \
+                   --nossl2                               \;
+  done < users
+}
+email_bradok() {
+  while read p; do
+          imapsync --host1 'sh-pro32.hostgator.com.br'    \
+                   --user1 $p'@bradok.com.br'             \
+                   --password1 '@?SueTam#2' --ssl1 --sslargs1 "SSL_verify_mode=1"  \
+                   --host2 'localhost'                    \
+                   --user2 $p'.bradok.bkp'                \
+                   --password2 'Abc242526@2'              \
+                   --nossl2                               \;
+  done < users
+}
+email_mac() {
+  while read p; do
+          imapsync --host1 'sh-pro32.hostgator.com.br'    \
+                   --user1 $p'@mac-id.com.br'             \
+                   --password1 '@?SueTam#2' --ssl1 --sslargs1 "SSL_verify_mode=1"  \
+                   --host2 'localhost'                    \
+                   --user2 $p'.mac-id.bkp'                \
+                   --password2 'Abc242526@2'              \
+                   --nossl2                               \;
+  done < users
+}
+
 #---------------------- EXECUÇÃO ----------------------------------------- #
 
-checar_usuario
+case $1 in
+  -h) echo "$MENSAGEM_USO" && exit 0                   ;;
+  -v) echo "$VERSAO" && exit 0                         ;;
+  -d) CHAVE_DADY=1                                     ;;
+  -b) CHAVE_BRADOK=1                                   ;;
+  -c) CHAVE_MAC=1                                      ;;
+  -a) CHAVE_ALL=1                                      ;;
+   *) echo "Seleciona uma opção válida. Consulte o -h" ;;
+esac
+
+#[ CHAVE_DADY -eq 1 ] && email_dady() && exit 0 ;;
+#[ CHAVE_BRADOK -eq 1 ] && email_bradok && exit 0 ;;
+#[ CHAVE_MAC -eq 1 ] && email_mac() && exit 0 ;;
+
+if [ $CHAVE_ALL -eq 1 ]; then
+  email_dady
+  email_bradok
+  email_mac
+
+
+
 #------------------------------------------------------------------------- #
