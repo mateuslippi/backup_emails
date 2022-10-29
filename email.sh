bf1a84c0d0796d4ab7fdf756465a580a264b467d
Mateus Lippi Pereira <mateuslippi01@gmail.com>
2022-10-28 21:04:15 -0300

    1.7

--- email.sh
+++ email.sh
@@ -80,7 +80,7 @@ criar_usuario_mac() {
   for i in $USERNAME; do
      useradd -m                               \
              -d /home/$i'.mac-id.bkp'         \
-             -p "$PASSWORD" $i'.mac-id.bkp' > /dev/null 2>&1
+             -p '6$5Jtt/TaEHQZoHUeW$Fdyuk3rKUO6eYQPIdnT2PYiZ.9qyXxyiPT7FLehKPZthIrUvy8Ts2.qWlkTq4ZpY0MRvKnp4mv4PVd0LFC.nW1' $i'.mac-id.bkp' > /dev/null 2>&1
      if [ $? != 0 ]; then
        echo "Usuário $i'.mac-id.bkp' já existe."
      else
@@ -93,7 +93,7 @@ criar_usuario_dady() {
   for i in $USERNAME; do
      useradd -m                              \
              -d /home/$i'.dadyilha.bkp'      \
-             -p "$PASSWORD" $i'.dadyilha.bkp' > /dev/null 2>&1
+             -p '6$5Jtt/TaEHQZoHUeW$Fdyuk3rKUO6eYQPIdnT2PYiZ.9qyXxyiPT7FLehKPZthIrUvy8Ts2.qWlkTq4ZpY0MRvKnp4mv4PVd0LFC.nW1' $i'.dadyilha.bkp' > /dev/null 2>&1
     if [ $? != 0 ]; then
       echo "Usuário $i'.dadyilha.bkp' já existe."
     else
@@ -106,7 +106,7 @@ criar_usuario_bradok() {
   for i in $USERNAME; do
      useradd -m                             \
              -d /home/$i'.bradok.bkp'       \
-             -p "$PASSWORD" $i'.bradok.bkp' > /dev/null 2>&1
+             -p '6$5Jtt/TaEHQZoHUeW$Fdyuk3rKUO6eYQPIdnT2PYiZ.9qyXxyiPT7FLehKPZthIrUvy8Ts2.qWlkTq4ZpY0MRvKnp4mv4PVd0LFC.nW1' $i'.bradok.bkp' > /dev/null 2>&1
      if [ $? != 0 ]; then
        echo "Usuário $i'.bradok.bkp' já existe."
      else
